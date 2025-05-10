d68f9b8b84b7baa4837c5602caec60e84f99bff1
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2025-05-10 20:25:34 +0530

    Added port

--- index.js
+++ index.js
@@ -34,4 +34,6 @@ app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.listen(3000, () => console.log("listening on 3000"));
+app.listen(process.env.PORT, () =>
+	console.log("app listening on ", process.env.PORT)
+);
